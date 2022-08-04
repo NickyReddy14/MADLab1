b7969fbf1b322fecf4eed84c29165ed6ec2c3598
M Joseph Nikhil Reddy <nikhilmaramreddy@gmail.com>
2022-08-04 15:31:27 +0530

    Commit

--- app/src/main/java/com/ninto/app1/MainActivity.java
+++ app/src/main/java/com/ninto/app1/MainActivity.java
@@ -2,7 +2,9 @@ package com.ninto.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Size;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText num1;
     private EditText num2;
     private Button btn;
+    private Button clch;
+    private Button szch;
     private TextView text;
 
     @Override
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         num2 = (EditText)findViewById(R.id.n2);
         btn = (Button)findViewById(R.id.b1);
         text = (TextView)findViewById(R.id.t);
+        clch = (Button)findViewById(R.id.clr);
+        szch = (Button)findViewById(R.id.sze);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +37,27 @@ public class MainActivity extends AppCompatActivity {
                 int y = Integer.parseInt(num2.getText().toString());
                 int sum = x+y;
                 text.setText(String.valueOf(sum));
+            }
+        });
+
+        clch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //switch(v.getId()) {
+                    //case R.id.clr:
+                        text.setTextColor(Color.parseColor("#FF0000"));
+                        /*break;
+                    case R.id.sze:
+                        text.setTextSize(60);
+                        break;
+                    default: break;
+                }*/
+            }
+        });
+        szch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                text.setTextSize(60);
             }
         });
     }
